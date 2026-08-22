1d0f6550d6f3c994e6103f0b073f45a34e314a54
Hakerhakb11 <igorshilov2007@gmail.com>
2026-08-22 19:53:13 +0600

    fix: get_user_config.py path bug fixed

--- src/engram_forge/get_user_config.py
+++ src/engram_forge/get_user_config.py
@@ -1,15 +1,20 @@
+import os
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).resolve().parent.parent.parent
+
 def get_name() -> str:
-    with open("config/my_name.txt", "r", encoding="utf-8") as f:
+    with open(os.path.join(PROJECT_DIR, "config/my_name.txt"), "r", encoding="utf-8") as f:
         return f.read().strip()
 
 
 def get_base_prompt() -> str:
-    with open("config/base_system_prompt.txt", "r", encoding="utf-8") as f:
+    with open(os.path.join(PROJECT_DIR, "config/base_system_prompt.txt"), "r", encoding="utf-8") as f:
         return f.read().strip()
 
 
 def get_chat_prompt() -> str:
-    with open("config/chat_system_prompt.txt", "r", encoding="utf-8") as f:
+    with open(os.path.join(PROJECT_DIR, "config/chat_system_prompt.txt"), "r", encoding="utf-8") as f:
         return f.read().strip()
 
 def get_prompt_for_chatting() -> str:
